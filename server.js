b5c688069b757f3985e38b75b87ad112641eec79
anurag <anurag9802@gmail.com>
2021-08-28 12:43:57 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const path = require("path");
+
+const connectDB = require("./server/database/connection");
+
+const app = express();
+
+dotenv.config({ path: "config.env" });
+const PORT = process.env.PORT || 8080;
+
+
+//mongoDB connection 
+connectDB();
+
+//log requests
+app.use(morgan("tiny"));
+
+//parse request to body parser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+//viewengine
+app.set("view engine", "ejs");
+app.set("viwes", "./views");
+
+//load assets
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+
+//load routers
+app.use("/", require("./server/routes/router"));
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on http://localhost:${PORT}`);
+});
