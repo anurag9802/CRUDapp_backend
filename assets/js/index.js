1c884b5630d5d1b8c0014e5ab7279af46c204bb6
anurag <anurag9802@gmail.com>
2021-09-02 13:04:30 +0530

    updated index.js again

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,48 +1,43 @@
-$("#add_user").submit(function(event){
-    alert("Data Inserted Successfully!");
-})
+$("#add_user").submit(function (event) {
+  alert("Data Inserted Successfully!");
+});
 
-$("#update_user").submit(function(event){
-    event.preventDefault();
+$("#update_user").submit(function (event) {
+  event.preventDefault();
 
-    var unindexed_array = $(this).serializeArray();
-    var data = {}
+  var unindexed_array = $(this).serializeArray();
+  var data = {};
 
-    $.map(unindexed_array, function(n, i){
-        data[n['name']] = n['value']
-    })
+  $.map(unindexed_array, function (n, i) {
+    data[n["name"]] = n["value"];
+  });
 
+  var request = {
+    url: `https://crud-app-mongo.herokuapp.com/api/users/${data.id}`,
+    method: "PUT",
+    data: data,
+  };
+
+  $.ajax(request).done(function (response) {
+    alert("Data Updated Successfully!");
+  });
+});
+
+if (window.location.pathname == "/") {
+  $ondelete = $(".table tbody td a.delete");
+  $ondelete.click(function () {
+    var id = $(this).attr("data-id");
 
     var request = {
-        // "url" : `http://localhost:3000/api/users/${data.id}`,
-        "url" : `https://crud-app-mongo.herokuapp.com/api/users/${data.id}`,
-        "method" : "PUT",
-        "data" : data
+      url: `https://crud-app-mongo.herokuapp.com/api/users/${id}`,
+      method: "DELETE",
+    };
+
+    if (confirm("Do you really want to delete this record?")) {
+      $.ajax(request).done(function (response) {
+        alert("Data Deleted Successfully!");
+        location.reload();
+      });
     }
-
-    $.ajax(request).done(function(response){
-        alert("Data Updated Successfully!");
-    })
-
-})
-
-if(window.location.pathname == "/"){
-    $ondelete = $(".table tbody td a.delete");
-    $ondelete.click(function(){
-        var id = $(this).attr("data-id")
-
-        var request = {
-            // "url" : `http://localhost:3000/api/users/${id}`,
-            "url" : `https://crud-app-mongo.herokuapp.com/api/users/${data.id}`,
-            "method" : "DELETE"
-        }
-
-        if(confirm("Do you really want to delete this record?")){
-            $.ajax(request).done(function(response){
-                alert("Data Deleted Successfully!");
-                location.reload();
-            })
-        }
-
-    })
+  });
 }
